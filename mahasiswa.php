bee6dd4074e9e46229fb3b4079b71b868da45fbb
adeliaeka <adellia3210@gmail.com>
2026-06-18 12:13:25 +0700

    change: php code

--- mahasiswa.php
+++ mahasiswa.php
@@ -1,3 +1,26 @@
+<?php
+
+//API
+$koneksi = mysqli_connect("localhost", "root", "", "adlweekly");
+
+if (!$koneksi) {
+    die("Koneksi gagal: " . mysqli_connect_error());
+}
+
+$query = "SELECT * FROM mahasiswa";
+
+$result = mysqli_query($koneksi, $query); //// lemari isi data
+
+
+
+////ambil data (fetch) dari lemari
+///mysqli_fetch_row
+///mysqli_fetch_assoc
+///mysqli_fetch_array
+///mysqli_fetch_object
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -5,26 +28,29 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;600&display=swap" rel="stylesheet">
 </head>
 
 <body>
     <h1 align="center">
         WEB TI UNIMUS 2026 OYEE
     </h1>
-    <table border="1" align="center" cellspacing="0" cellpadding="10px">
-        <nav class="navbar">
-            <a href="index.php">Home</a>
-            <a href="biodata.php">Biodata</a>
-            <a href="kontak.php">Kontak</a>
-            <a href="mahasiswa.php">Data Mahasiswa</a>
-        </nav>
-    </table>
+
+    <nav class="navbar">
+        <a href="index.php">Home</a>
+        <a href="biodata.php">Biodata</a>
+        <a href="kontak.php">Kontak</a>
+        <a href="mahasiswa.php">Data Mahasiswa</a>
+    </nav>
+
     <h2>
         Data Mahasiswa
     </h2>
     <div class="btn-container">
         <a href="tambahdata.php" class="btn">+ Tambah Data</a>
     </div>
+
     <table border="1" cellpadding="5px">
         <tr>
             <th>No</th>
@@ -35,37 +61,31 @@
             <th>No. HP</th>
             <th>Foto</th>
             <th>Aksi</th>
-
-            <!-- <td>Baris 1, kolom2</td> -->
         </tr>
-        <tr>
-            <td align="center">1</td>
-            <td>Adelia eka rahmawati</td>
-            <td>13242520036</td>
-            <td align="center">Teknologi Informasi</td>
-            <td align="center">adellia3210@gmail.com</td>
-            <td align="center">0898772537354</td>
-            <td><img src="assets/img/foto.jpg" width="70px"></td>
-            <td>
-                <a href="editdata.php"><button>Edit</button></a>
-                <a href="deletdata.php"><button>Hapus</button></a>
-            </td>
-        </tr>
-        <tr>
-            <td align="center">2</td>
-            <td>Ghania Hafsha</td>
-            <td>13242520036</td>
-            <td align="center">Teknologi Informasi</td>
-            <td align="">ghania321@gmail.com</td>
-            <td align="center">08983321427</td>
-            <td><img src="assets/img/foto2.jpg" width="70px"></td>
-             <td>
-                <a href="editdata.php"><button>Edit</button></a>
-                <a href="deletdata.php"><button>Hapus</button></a>
-            </td>
-        </tr>
+        <?php
+        $no = 1;
+        while ($mhs = mysqli_fetch_assoc($result)) {
+        ?>
+            <tr>
+                <td align="center"><?php echo $no++; ?></td>
+                <td><?php echo $mhs["nama"]; ?></td>
+                <td><?php echo $mhs["nim"]; ?></td>
+                <td align="center"><?php echo $mhs["jurusan"]; ?></td>
+                <td align="center"><?php echo $mhs["email"]; ?></td>
+                <td align="center"><?php echo $mhs["no_hp"]; ?></td>
+                <td><img src="assets/img/<?php echo $mhs["foto"]; ?>" width="70px"></td>
+                <td>
+                    <a href="editdata.php?id=<?php echo $mhs["id"]; ?>"><button>Edit</button></a>
+                    <a href="deletdata.php?id=<?php echo $mhs["id"]; ?>"><button>Hapus</button></a>
+                </td>
+            </tr>
+        <?php
+        }
+        ?>
     </table>
+
     <hr>
+
     <table border="1" cellpadding="5px">
         <tr>
             <td>1,1</td>
@@ -76,13 +96,10 @@
         <tr>
             <td>2,1</td>
             <td colspan="2" rowspan="2"></td>
-            <!-- <td>2,3</td> -->
             <td>2,4</td>
         </tr>
         <tr>
             <td>3,1</td>
-            <!-- <td>3,2</td> -->
-            <!-- <td>3,3</td> -->
             <td>3,4</td>
         </tr>
         <tr>
@@ -92,9 +109,6 @@
             <td>4,4</td>
         </tr>
     </table>
-
-    <link rel="stylesheet" href="assets/css/style.css">
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;600&display=swap" rel="stylesheet">
 
 </body>
 
